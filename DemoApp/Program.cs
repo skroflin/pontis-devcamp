e8c4eac967e9326b49eb19c0569fafb2908cf1e2
svenk <sven.kroflin@gmail.com>
2024-09-11 22:32:01 +0200

    #8 Finishing Core solution and starting Api solution.

--- DemoApp/Program.cs
+++ DemoApp/Program.cs
@@ -1,25 +1,21 @@
+using DemoApp.Core;
+using DemoApp.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddPersistence(builder.Configuration);
+builder.Services.AddCore(builder.Configuration);
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 app.UseHttpsRedirection();
-
+app.UseHsts();
+app.UseCookiePolicy();
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
