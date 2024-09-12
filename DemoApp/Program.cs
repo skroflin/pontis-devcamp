d69750be042cdbf82e34c1908206ba4bb7c16398
svenk <sven.kroflin@gmail.com>
2024-09-12 12:12:41 +0200

    #2 Added Serilog and Swagger UI.

--- DemoApp/Program.cs
+++ DemoApp/Program.cs
@@ -1,15 +1,56 @@
-using DemoApp.Core;
+﻿using DemoApp.Core;
 using DemoApp.Persistence;
+using DemoApp.Utilities.SecurityManagement;
+using Microsoft.OpenApi.Models;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<AccessOptions>(opt => builder.Configuration.Bind(nameof(AccessOptions), opt));
+
+builder.Host
+    .UseSerilog((hostingContext, services, loggerConfiguration) => loggerConfiguration
+    .ReadFrom.Configuration(hostingContext.Configuration)
+    .Enrich.FromLogContext()
+    .WriteTo.Console());
 
 builder.Services.AddPersistence(builder.Configuration);
 builder.Services.AddCore(builder.Configuration);
 
 builder.Services.AddControllers();
 builder.Services.AddRouting(options => options.LowercaseUrls = true);
+builder.Services.AddSwaggerGen(options =>
+{
+    #region Security definition
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "Najjača aplikacija u galaksiji i šire API!", Version = "v1", Description = "Ovo je najjača aplikacija u galaksiji!" });
+    options.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme()
+    {
+        Type = SecuritySchemeType.ApiKey,
+        In = ParameterLocation.Header,
+        Name = "X-Api-Key",
+        Description = "Specify API Key",
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "ApiKey" }
+            },
+            Array.Empty<string>()
+        }
+    });
+    #endregion
+});
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 app.UseHsts();
