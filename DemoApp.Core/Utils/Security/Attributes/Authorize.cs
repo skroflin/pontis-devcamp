8730a832256687dcf0405e8e9a00c4ff4b1c3d15
svenk <sven.kroflin@gmail.com>
2024-09-13 10:12:14 +0200

    #5 Custom Authorize attribute.

---
+++ DemoApp.Core/Utils/Security/Attributes/Authorize.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace DemoApp.Core.Utils.Security.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class AuthorizeAttribute : Attribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var allowAnonymous = context.ActionDescriptor.FilterDescriptors.Any(fd => fd.Filter is AllowAnonymousAttribute);
+            if(allowAnonymous)
+                return;
+
+            var xApiKey = context.HttpContext.Items["XApiKey"]?.ToString();
+            if(xApiKey == null)
+            {
+                throw new UnauthorizedAccessException();
+            }
+        }
+    }
+}
